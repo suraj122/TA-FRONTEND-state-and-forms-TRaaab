8308a634063575ab8130ea1e0be6085c7a2d3276
suraj_122 <122suraj0@gmail.com>
2022-08-22 20:44:03 +0530

    build app for handling multiple inputs

---
+++ block-BRaabr/code/src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import MultipleInput from "./MultipleInput";
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Form Validation</h1>
+        <MultipleInput />
+      </>
+    );
+  }
+}
+
+export default App;
